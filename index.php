195cc4393ae8f818d29dd579449e977f601184e6
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2025-06-06 14:43:38 -0400

    Refactoring routes in index.php

--- index.php
+++ index.php
@@ -1,32 +1,24 @@
 <?php
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestUri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $method = $_SERVER['REQUEST_METHOD'];
-
-$requestUri = rtrim($requestUri, '/');
 
 if ($requestUri === '' || $requestUri === '/api') {
     echo json_encode(['message' => 'API root']);
     exit;
 }
 
-if (str_starts_with($requestUri, '/api/contas')) {
-    // api/contas ou /api/contas/123
-    $parts = explode('/', $requestUri);
-    $id = $parts[3] ?? null;
+$routes = [
+    '/api/contas' => '/api/contas.php',
+    '/api/transacoes' => '/api/transacoes.php',
+];
 
-    require __DIR__ . '/api/contas.php';
-
-
-    exit;
-}
-
-if (str_starts_with($requestUri, '/api/transacoes')) {
-    $parts = explode('/', $requestUri);
-    $id = $parts[3] ?? null;
-
-    require __DIR__ . '/api/transacoes.php';
-
-    exit;
+foreach ($routes as $route => $file) {
+    if (str_starts_with($requestUri, $route)) {
+        $parts = explode('/', $requestUri);
+        $_GET['id'] = $parts[3] ?? null;
+        require __DIR__ . $file;
+        exit;
+    }
 }
 
 http_response_code(404);
